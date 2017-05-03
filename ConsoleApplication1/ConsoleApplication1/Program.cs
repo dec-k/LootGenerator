84da962acc37246847203469094edbae3a582521
Dec Keighley <keigdl1@student.op.ac.nz>
2017-05-03 18:33:46 +1200

    Ver. 0.2.0, now generating lists from plug-and-play text files in .../bin/debug/

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -14,166 +14,30 @@ namespace LootGenerator
 
         static void Main(string[] args)
         {
+            //Initialise the word lists
             prefixes = new List<string>();
-            prefixes.Add("Rotten");
-            prefixes.Add("Infested");
-            prefixes.Add("Diseased");
-            prefixes.Add("Blazing");
-            prefixes.Add("Fiery");
-            prefixes.Add("Demonic");
-            prefixes.Add("Hellish");
-            prefixes.Add("Diabolic");
-            prefixes.Add("Divine");
-            prefixes.Add("Pure");
-            prefixes.Add("Ascended");
-            prefixes.Add("Martyr's");
-            prefixes.Add("Savant's");
-            prefixes.Add("Nihilist's");
-            prefixes.Add("Savage");
-            prefixes.Add("Brutal");
-            prefixes.Add("Vicious");
-            prefixes.Add("Extreme");
-            prefixes.Add("Fine");
-            prefixes.Add("Exceptional");
-            prefixes.Add("Masterworked");
-            prefixes.Add("Honed");
-            prefixes.Add("Precise");
-            prefixes.Add("Rampaging");
-            prefixes.Add("Chaotic");
-            prefixes.Add("Nightmarish");
-            prefixes.Add("Ghoulish");
-            prefixes.Add("Hardened");
-            prefixes.Add("Attuned");
-            prefixes.Add("Balanced");
-            prefixes.Add("Heavy");
-            prefixes.Add("Light-Weight");
-            prefixes.Add("Profane");
-            prefixes.Add("Enigmatic");
-            prefixes.Add("Blighted");
-            prefixes.Add("Mysterious");
-            prefixes.Add("Sacred");
-            prefixes.Add("Eclipsing");
-            prefixes.Add("Dark");
-            prefixes.Add("Perfect");
-            prefixes.Add("Flawed");
-            prefixes.Add("Broken");
-            prefixes.Add("Useless");
-            prefixes.Add("Remarkable");
-            prefixes.Add("Cracked");
-            prefixes.Add("Chipped");
-            prefixes.Add("Bloody");
-            prefixes.Add("Terrifying");
-            prefixes.Add("Grim");
-            prefixes.Add("Blood-Caked");
-            prefixes.Add("Amateur's");
-            prefixes.Add("Forgettable");
-            prefixes.Add("Mediocre");
-            prefixes.Add("Intimidating");
-
             types = new List<string>();
-            types.Add("Straight Sword");
-            types.Add("Dagger");
-            types.Add("Falchion");
-            types.Add("Greatsword");
-            types.Add("Bastard Sword");
-            types.Add("Mail-Breaker");
-            types.Add("Dirk");
-            types.Add("Knife");
-            types.Add("Gladius");
-            types.Add("Scimitar");
-            types.Add("Axe");
-            types.Add("Battleaxe");
-            types.Add("Great Axe");
-            types.Add("Hammer");
-            types.Add("Warhammer");
-            types.Add("Scythe");
-            types.Add("Mace");
-            types.Add("Flanged Mace");
-            types.Add("Flail");
-            types.Add("Spear");
-            types.Add("Trident");
-            types.Add("Scepter");
-            types.Add("Wand");
-            types.Add("Naginata");
-            types.Add("Cestus");
-            types.Add("Knuckle");
-            types.Add("Katar");
-            types.Add("Flail");
-            types.Add("Bow");
-            types.Add("Greatbow");
-            types.Add("Crossbow");
-            types.Add("Hand-Cannon");
-            types.Add("Bandit's Dagger");
-            types.Add("Throwing Axe");
-            types.Add("Boomerang");
-            types.Add("Small Shield");
-            types.Add("Wooden Shield");
-            types.Add("Buckler");
-            types.Add("Kite Shield");
-            types.Add("Greatshield");
-            types.Add("Pan Lid");
-            types.Add("Pitchfork");
-            types.Add("Hand Axe");
-            types.Add("Shotel");
-            types.Add("Flamberge");
-            types.Add("Morning Star");
-
             suffixes = new List<string>();
-            suffixes.Add(", the Pustulent");
-            suffixes.Add("of Might");
-            suffixes.Add("of Annihilation");
-            suffixes.Add("of the Damned");
-            suffixes.Add("of Sanctity");
-            suffixes.Add(", the Untested");
-            suffixes.Add(", the Favoured");
-            suffixes.Add(", the Warbringer");
-            suffixes.Add(", the Truth-Seeker");
-            suffixes.Add(", the Ruin");
-            suffixes.Add("of the Fifth Sigil");
-            suffixes.Add("of the 38 Knights");
-            suffixes.Add("of the Favoured");
-            suffixes.Add("of Undead-Slaying");
-            suffixes.Add("of Beast-Culling");
-            suffixes.Add("of Man-Slaying");
-            suffixes.Add("the Bizarre");
-            suffixes.Add("the Uncanny");
-            suffixes.Add("of Nightmares");
-            suffixes.Add("of Hell");
-            suffixes.Add("of Suffering");
-            suffixes.Add("of the Abyss");
-            suffixes.Add("of Restoration");
-            suffixes.Add("of Miracles");
-            suffixes.Add("of Hell's Armies");
-            suffixes.Add("of Remorselessness");
-            suffixes.Add("of Regret");
-            suffixes.Add("of Hopelessness");
-            suffixes.Add("of Life-Draining");
-            suffixes.Add("of Brutality");
-            suffixes.Add("of Savagery");
-            suffixes.Add("of Sadism");
-            suffixes.Add("of Masochism");
-            suffixes.Add("of the Harsh Trials");
-            suffixes.Add("of Legends");
-            suffixes.Add("of Distinction");
-            suffixes.Add("of Uselessness");
-            suffixes.Add(", the Unremarkable");
-            suffixes.Add("of Distinction");
-            suffixes.Add(", Cradle of Hope");
-            suffixes.Add(", Hell's Bane");
-            suffixes.Add(", Skywarden");
-            suffixes.Add(", the Sylphid Breeze");
-            suffixes.Add(", the Loyal Servant");
-            suffixes.Add(", the Fearsome Tyrant");
-            suffixes.Add(", the Godbreaker");
-            suffixes.Add(", Thunderforged Nemesis");
-            suffixes.Add(", Crucible of the Forgotten");
-            suffixes.Add(", Hand of the Deep");
+
+            //Populate the lists with data retrieved from txt files in .../bin/debug/
+            fetchWordsFromFile(prefixes, "prefixes.txt");
+            fetchWordsFromFile(types, "types.txt");
+            fetchWordsFromFile(suffixes, "suffixes.txt");
 
             bool restarting = startCycle();
 
             while (restarting)
             {
                 restarting = startCycle();
+            }
+        }
+
+        private static void fetchWordsFromFile(List<string> list, string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                list.Add(line);
             }
         }
 
